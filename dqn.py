d61dec196d955b3a34a82f5e1462005497d962a5
kanokkorn <kanokorn@outlook.jp>
2025-12-29 20:06:33 +0700

    re-implement dqn from https://docs.pytorch.org/tutorials/intermediate/reinforcement_q_learning.html

--- dqn.py
+++ dqn.py
@@ -1,43 +1,105 @@
-from tinygrad import Tensor
+import os
+import time
+import random
+from collections import deque
+from typing import Tuple
+
+from tinygrad import Tensor, nn, TinyJit
 from tinygrad import Device
+from tinygrad.helpers import trange
+from tinygrad.nn.state import safe_save, safe_load, get_state_dict, load_state_dict
+
+import gymnasium as gym
+
+# hyperparams
+ENVIRONMENT_NAME = 'CartPole-v1'
+
+BATCH_SIZE = 256
+ENTROPY_SCALE = 0.0005
+REPLAY_BUFFER_SIZE = 10000
+PPO_EPSILON = 0.2
+HIDDEN_UNITS = 32
+LEARNING_RATE = 1e-2
+TRAIN_STEPS = 5
+EPISODES = 1000
+DISCOUNT_FACTOR = 0.99
 
 
-# set of actions + states
-transtion = {}
-
-def test():
-  print("begintest")
-  x = Tensor.eye(3, requires_grad=True)
-  y = Tensor([[2.0,0,-2.0]], requires_grad=True)
-  z = y.matmul(x).sum()
-  z.backward()
-  print(x.grad.tolist())  # dz/dx
-  print(y.grad.tolist())  # dz/dy
-  print("endtest")
-
-# TODO: DQN implementation :: https://en.wikipedia.org/wiki/Q-learning#Deep_Q-learning
-# how complex it should be to fit most training environment?
 class DQN:
-  def __init__(self):
-    self.l1 = Tensor.kaiming_uniform(784, 128)
-    self.l2 = Tensor.kaiming_uniform(128, 10)
-    self.l3 = Tensor.kaiming_uniform(128, 10)
-    self.l3 = Tensor.kaiming_uniform(128, 10)
-    self.l3 = Tensor.kaiming_uniform(128, 10)
-  def __call__(self, x: Tensor) -> Tensor:
-    return x.flatten(1). dot(self.l1).relu().dot(self.l2)
+  def __init__(self, state_dim, action_dim, hidden_dim=256):
+    self.l1 = nn.Linear(state_dim, hidden_dim)
+    self.l2 = nn.Linear(hidden_dim, action_dim)
+
+  def __call__(self, x):
+    x = self.l1(x).relu()
+    return self.l2(x)
 
 class ExperienceReplay:
-  def __init__(self):
-    self.memory = deque([], maxlen=capacity)
-  def push(self, *args):
-    self.memory.append(transtion(*args))
-  def sample(self, batch_size):
-    return random.sample(self.memory, batch_size)
+  def __init__(self, maxlen, seed=None):
+    self.memory = deque([], maxlen=maxlen)
+    if seed is not None:
+      random.seed(seed)
+
+  def append(self, transition):
+    self.memory.append(transition)
+
+  def sample(self, sample_size):
+    return rando.sample(self.memory, sample_size)
+
   def __len__(self):
     return len(self.memory)
 
+class Agent:
+  def run(self, is_training=True, render=False):
+    env = gym.make(ENVIRONMENT_NAME)
+    num_state = env.observation_space.shape[0]
+    num_action = env.action_space.n
+    policy_dqn = DQN(num_state, num_action)
+    state_dict = get_state_dict(model)
+    reward_per_episode = []
+    try:
+      state_dict = safe_load('checkpoint.safetensors')
+      load_state_dict(model, state_dict)
+      print("checkpoint loaded..")
+    except FileNotFoundError:
+      print("checkpoint not found, creating new one..")
+      safe_save(state_dict, 'checkpoint.safetensors')
+    if is_training:
+      memory = ExperienceReplay(REPLAY_BUFFER_SIZE)
+      policy_dqn = DQN(num_state, num_action)
+      state_dict = get_state_dict(policy_dqn)
+      try:
+        state_dict = safe_load('checkpoint.safetensors')
+        load_state_dict(model, state_dict)
+        print("checkpoint loaded..")
+      except FileNotFoundError:
+        print("checkpoint not found, creating new one..")
+        safe_save(state_dict, 'checkpoint.safetensors')
+    for episode in range(EPISODES):
+      obs, _ = env.reset()
+      terminated = False
+      episode_reward = 0.0
+      while not terminated:
+        action = env.action_space.sample()
+        obs, reward, terminated, _, info = env.step(action)
+        episode_reward
+        if is_training:
+          memory.append((state, action, reward, terminated))
+        state = obs
+      reward_per_episode.append(episode_reward)
+    env.close()
+
+@TinyJit
+def jit(x):
+  return net(x).realize()
+
+def intialize_env():
+  env = gym.make(ENVIRONMENT_NAME)
+  env.action_space
+  env.observation_space
+  print(f"{ENVIRONMENT_NAME} have {env.action_space.n} action(s) and {env.observation_space} observe(s)")
+  print(f"Running on device {Device.DEFAULT}")
+
+
 if __name__ == "__main__":
-  print("Running on " + Device.DEFAULT)
-  test()
-  model = DQN()
+  intialize_env()
